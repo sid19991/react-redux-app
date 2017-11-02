8169f2775bb9f4e154de1617d7bcd164d01280f9
Siddharth <siddharthpogul1999@gmail.com>
2017-11-02 22:23:03 +0530

    added project files

---
+++ dev/js/containers/user-detail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {connect} from 'react-redux';
+class UserDetail extends React.Component{
+  render(){
+if(!this.props.user){
+  return(<h2>Select a User...</h2>)
+}
+    return(
+      <div>
+    <img src={this.props.user.thumbnail}/>
+    <h2>{this.props.user.first} {this.props.user.last}</h2>
+    <h3>Age: {this.props.user.age}</h3>
+    <h3>Description: {this.props.user.description}</h3>
+    </div>)
+  }
+}
+function mapStateToProps(state){
+  return {
+    user:state.activeUser
+  }
+}
+export default connect(mapStateToProps)(UserDetail)
---
+++ dev/js/index.js
@@ -0,0 +1,16 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import allReducers from './reducers/index';
+import App from './components/App';
+const store = createStore(allReducers);
+
+ReactDOM.render(
+<Provider store={store}>
+<App/>
+</Provider>
+    ,
+    document.getElementById('root')
+);
